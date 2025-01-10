f8b9ffaabee5a25b031907ae110dc31d5f3f2dd2
Moin Shahid <moinshahid919@gmail.com>
2025-01-10 16:59:22 +0500

    Update files

--- test/Dutch_Auction.t.sol
+++ test/Dutch_Auction.t.sol
@@ -6,4 +6,14 @@ import {DutchAuction} from "../src/Dutch_Auction.sol";
 
 contract AuctionTest is Test {
     DutchAuction public auction;
+    address public nft=0xf26Fc56ECbE58Ca9f95Cc4dB1D8986d98439F065;
+
+    function testInitialSetup() public view {
+    assertEq(auction.seller(), address(this));
+    assertEq(auction.startingPrice(), 7000 ether);
+    assertEq(auction.discountRate(), 0.01 ether);
+    assertEq(auction.nftId(), 1);
+    assertTrue(address(nft) == address(auction.nft()));
+}
+
 }
